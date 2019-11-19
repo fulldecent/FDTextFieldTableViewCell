7037d1ce1147137c1ce57e7a1893f3ced850bdce
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2019-11-19 21:19:50 +0200

    Update project layout.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,24 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-  name: "FDTextFieldTableViewCell"
+    name: "FDTextFieldTableViewCell",
+    products: [
+        .library(
+            name: "FDTextFieldTableViewCell",
+            targets: ["FDTextFieldTableViewCell"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "FDTextFieldTableViewCell",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "FDTextFieldTableViewCellTests",
+            dependencies: ["FDTextFieldTableViewCell"]
+        )
+    ]
 )
